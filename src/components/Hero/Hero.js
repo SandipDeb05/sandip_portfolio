b5c0949bfd97f47c852ced80a1a62dbf88c4a4c2
SandipDeb <sandip.deb@xopuntech.com>
2022-05-30 11:14:01 +0530

    header email link is fixed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,6 +8,10 @@ import {
 import Button from "../../styles/GlobalComponents/Button";
 import { LeftSection } from "./HeroStyles";
 
+const mailTo = () => {
+  location.href = "mailto:sandipdeb05@gmail.com";
+};
+
 const Hero = () => (
   <Section row nopadding>
     <LeftSection>
@@ -19,11 +23,7 @@ const Hero = () => (
         A Full-Stack Developer mainly focusing on Frontend Technologies like
         React, Redux, Next, and many more...
       </SectionText>
-      <Button>
-        <a href="mailto:sandipdeb05@gmail.com" style={{ color: "#fff" }}>
-          Email Now
-        </a>
-      </Button>
+      <Button onClick={mailTo}>Email Now</Button>
     </LeftSection>
   </Section>
 );
