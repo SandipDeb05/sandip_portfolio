f55c548de81a8e7fbad68893cf434bdc15bf8938
SandipDeb <sandipdeb05@gmail.com>
2023-05-20 22:34:35 +0530

    about us updated

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -13,19 +13,13 @@ const Timeline = () => {
         I specialize in React and the MERN stack, which includes MongoDB,
         Express, React, and Node. With years of experience in software
         development, I have honed my expertise in creating dynamic and
-        responsive web applications using these technologies. In my current role
-        with a startup company, I am working in a small team to develop a React
-        project from scratch, which allowed me to enhance my knowledge of the
-        React ecosystem and JavaScript. I am passionate about building
-        innovative and efficient applications that meet the specific needs of my
-        clients. I have a proven track record of developing top-quality web
-        applications and managing projects from conception to launch. My
-        technical proficiency, combined with my ability to work effectively in a
-        team, enables me to create solutions that are scalable, secure, and
-        high-performing. I am always learning and expanding my knowledge of new
-        technologies and industry best practices. If you're looking for a
-        skilled and motivated developer with experience in the MERN stack and
-        full-stack development, I am the right person for the job. <br />
+        responsive web applications using these technologies. I have a proven
+        track record of developing top-quality web applications and managing
+        projects from conception to launch. My technical proficiency, combined
+        with my ability to work effectively in a team, enables me to create
+        solutions that are scalable, secure, and high-performing. I am always
+        learning and expanding my knowledge of new technologies and industry
+        best practices. <br />
         Technology Stack: HTML, CSS, JavaScript, React, Redux, React-Router,
         Next, MERN stack.
       </SectionText>
